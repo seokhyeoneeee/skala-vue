4f883666836ae79668de02c7805aa2e5e3a8978f
seokhyeon <5e0khyun.ch@gmail.com>
2026-08-27 17:04:08 +0900

    feat: 과제 1 날씨 Mockup 구현

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import WeatherMockup from './components/exercise/WeatherMockup.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <h1>⛅ 과제 1: 날씨 (Mockup)</h1>
+    <hr />
+    <WeatherMockup />
+  </div>
+</template>
+
+<style>
+@import '@/assets/exercise.css';
+</style>
